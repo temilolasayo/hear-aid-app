1e697de1d809a228c40164ed055faa876694935f
temilolasayo <alajikitemilola@gmail.com>
2020-07-16 21:25:08 +0100

    Modified the heroku configuration

--- config.php
+++ config.php
@@ -1,9 +1,17 @@
 <?php
 //Database details
-$db_host = 'localhost';
-$db_username = 'shayo';
-$db_password = 'password123456';
-$db_name = 'test';
+
+mysql://b67456d4bbec5d:bce318a9@us-cdbr-east-02.cleardb.com/heroku_a4275d67572b90d?reconnect=true
+
+// $db_host = 'localhost';
+// $db_username = 'shayo';
+// $db_password = 'password123456';
+// $db_name = 'test';
+
+$db_host = 'us-cdbr-east-02.cleardb.com';
+$db_username = 'b67456d4bbec5d';
+$db_password = 'bce318a9';
+$db_name = 'heroku_a4275d67572b90d';
 
 //Create connection and select DB
 $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
