8ecea9807284401188ca6478d546f6ad265178ac
Alajiki Temilola <alajikitemilola@gmail.com>
2020-07-19 23:53:40 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
+
 //Database details
 
-
-$db_host = 'us-cdbr-east-02.cleardb.com';
-$db_username = 'b67456d4bbec5d';
-$db_password = 'bce318a9';
-$db_name = 'heroku_a4275d67572b90d';
+$servername='us-cdbr-east-02.cleardb.com';
+$username='b67456d4bbec5d';
+$password='bce318a9';
+$dbname = "heroku_a4275d67572b90d";
 
 //Create connection and select DB
-$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$conn=mysqli_connect($servername,$username,$password,"$dbname");
+if(!$conn){
+   die('Could not Connect My Sql:' .mysql_error());
 }
+?>
